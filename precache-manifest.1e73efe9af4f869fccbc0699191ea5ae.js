22e0967c5212eaf6c83de9e5b64eb0f1d64bdfd5
Mohammad <mohammadidehgamwala5@gmail.com>
2020-09-23 20:09:43 +0530

    Updates

---
+++ precache-manifest.1e73efe9af4f869fccbc0699191ea5ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c69d54f57740533d7caef29255b7c57",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f6681098b3886ddb495d",
+    "url": "/todo/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "dac5d613883b680901b8",
+    "url": "/todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f6681098b3886ddb495d",
+    "url": "/todo/static/js/2.3ab4ccf0.chunk.js"
+  },
+  {
+    "revision": "c53219b5224141e776f7518763c0b708",
+    "url": "/todo/static/js/2.3ab4ccf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac5d613883b680901b8",
+    "url": "/todo/static/js/main.9db69a5b.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
